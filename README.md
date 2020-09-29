cf03fd93069322abd0082473fa60ee2f4f8855b5
Will Richards <jwilt9000@gmail.com>
2020-09-29 11:18:16 -0700

    Added anaconda environment to the repo with instructions Updated to README.md to explain how it can be utilized,

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # FRC_AI
 
-Version 2 of my attempt to create a simulation environment for training a neural network driven FRC robot
+Version 2 of my attempt to create a simulation environment for training a neural network driven FRC robot.
+
+# Setup
+
+If you wish to use this without incident you will need to create an anaconda environment with the same packages.
+
+1. The exported conda environment is located in the project root titled
+`FRC_AI.yml`
+2. You can then generate a new environment by running 
+```shell script
+conda env create -f=env.yml
+```
--- Simulated Environment/Physics Engine/EasyPhysics.py
+++ Simulated Environment/Physics Engine/EasyPhysics.py
@@ -7,6 +7,7 @@ __copyright__ = "Copyright 2020, AEMBOT"
 
 import math
 import pymunk
+import arcade
 from LowLevelPhysics import *
 
 # Gravity to be used in the simulated environment, its None because dampening is used to regulate object speed
@@ -15,16 +16,22 @@ GRAVITY = (0, 0)
 
 class PhysicsEnvironment:
 
-    def __init__(self, window_width, window_height, step_length):
+    def __init__(self, window_width, window_height, simulation_accuracy, step_length):
         """
         Create general variables that will be used throughout the class
 
         :param window_width: Width of the entire window
         :param window_height: Height of the entire window
-        :param step_length: The amount of time in seconds to move the simulation forward (Smaller numbers = higher simulation accuracy)
+
+        :param simulation_accuracy: How accurately it will attempt to get the simulation (higher numbers = higher accuracy)
+        :param step_length: The amount of time in seconds to move the simulation forward, (Smaller numbers = higher simulation accuracy as updates are more frequent)
         """
 
-        self.physics_space = self.createPhysicsSpace(simulation_accuracy=35)
+        # Create a new physics space with a simulation accuracy of 40
+        self.physics_space = self.createPhysicsSpace(simulation_accuracy=simulation_accuracy)
+
+        # Create a list of static sprites to render as static physics objects
+        self.sprite_list: arcade.SpriteList[PhysicsSprite] = arcade.SpriteList()
 
         # Values for the width and Height of the window
         self.window_width = window_width
@@ -32,9 +39,11 @@ class PhysicsEnvironment:
 
         self.step_length = step_length
 
-    def createPhysicsSpace(self, simulation_accuracy):
+    @staticmethod
+    def createPhysicsSpace(simulation_accuracy):
         """
         Create the physics simulation environment for all the objects
+
         :return: The created physics space
         """
 
@@ -55,5 +64,25 @@ class PhysicsEnvironment:
         """
 
         self.physics_space.step(self.step_length)
+
+    def createRectangularObject(self, width, height, initialX, initialY, sprite_path):
+        """
+        Create a static physics sprite with the given properties and add it to the sprites list
+
+        :param width: The width of the physics bounding box
+        :param height: The height of the physics bounding box
+        :param initialX: The initial X location of the object
+        :param initialY: The initial Y location of the object
+        :param sprite_path: The file path of the sprite to use
+        :return: None
+        """
+
+        # Create a new static physics body
+        physics_body = pymunk.Body(body_type=pymunk.Body.STATIC)
+
+        # Set the initial position of the object
+        physics_body.position = pymunk.Vec2d(initialX, initialY)
+
+
 
 

ab98666c173f8560ee7540ade3944b3ff8b8c485
Will Richards <jwilt9000@gmail.com>
2020-09-30 11:35:56 -0700

    Made EasyPhysics.py easier to use 1. Split physics object creation into Static and Dynamic classes 2. Made the PhysicsEnvironment class and overall physics manager 3. Created a wrapper for BoxSprite to included dampening to make it a more full physics object

--- Simulated Environment/Physics Engine/Collision Handling/CollisionTypes.py
+++ Simulated Environment/Physics Engine/Collision Handling/CollisionTypes.py
@@ -2,7 +2,12 @@
 Enum of different collision types
 """
 
+__author__ = "Will Richards"
+__copyright__ = "Copyright 2020, AEMBOT"
+
 from enum import Enum
+
+
 class CollisionType(Enum):
     STATIC_OBJECT = 1
     DYNAMIC_OBJECT = 2
--- Simulated Environment/Physics Engine/EasyPhysics.py
+++ Simulated Environment/Physics Engine/EasyPhysics.py
@@ -5,9 +5,7 @@ High Level Physics Interface Meant To Be Used By The Environment Itself
 __author__ = "Will Richards"
 __copyright__ = "Copyright 2020, AEMBOT"
 
-import math
 import pymunk
-import arcade
 from LowLevelPhysics import *
 from CollisionTypes import CollisionType
 
@@ -16,6 +14,7 @@ GRAVITY = (0, 0)
 
 
 class PhysicsEnvironment:
+    """Class to manager the overall physics environment"""
 
     def __init__(self, window_width, window_height, simulation_accuracy, step_length):
         """
@@ -43,8 +42,7 @@ class PhysicsEnvironment:
 
         self.step_length = step_length
 
-    @staticmethod
-    def createPhysicsSpace(simulation_accuracy):
+    def createPhysicsSpace(self, simulation_accuracy):
         """
         Create the physics simulation environment for all the objects
 
@@ -60,7 +58,7 @@ class PhysicsEnvironment:
 
         return space
 
-    def simulationStep(self):
+    def simulateStep(self):
         """
         Move the simulation the amount of time set by the step_length variable
 
@@ -69,7 +67,38 @@ class PhysicsEnvironment:
 
         self.physics_space.step(self.step_length)
 
-    def createRectangularObject(self, width, height, initialX, initialY, collision_type: CollisionType, sprite_path):
+    def createCollisionHandler(self, firstCollisionType: CollisionType, secondCollisionType: CollisionType, callback):
+        """
+        Create a new collision handler watching for specific collisions
+
+        :param firstCollisionType: Collision type of one object
+        :param secondCollisionType: Collision type of the other object
+        :param callback: Function to be called when the objects collide (method name without parameters Eg. this.test)
+
+        :return: None
+        """
+
+        # Create a new handler watching for the specified collisions
+        collision_handler = self.physics_space.add_collision_handler(collision_type_a=firstCollisionType.value,
+                                                                     collision_type_b=secondCollisionType.value)
+
+        # Set the method to preform the callback to
+        collision_handler.post_solve = callback
+
+
+class StaticPhysics:
+    """Class to manage creation of static physics object within the environment"""
+
+    def __init__(self, physics_environment: PhysicsEnvironment):
+        """
+        Create a new manager for the static physical objects
+
+        :param physics_environment: Reference to the overarching physics manager
+        """
+        self.physic_environment = physics_environment
+
+    def createStaticRectangularObject(self, width, height, initialX, initialY, collision_type: CollisionType,
+                                      sprite_path):
         """
         Create a static physics sprite with the given properties and add it to the sprites list
 
@@ -79,6 +108,7 @@ class PhysicsEnvironment:
         :param initialY: The initial Y location of the object
         :param collision_type: Enum to represent the type of object it is and in turn how it should handle collisions
         :param sprite_path: The file path of the sprite to use
+
         :return: None
         """
 
@@ -89,19 +119,22 @@ class PhysicsEnvironment:
         physics_body.position = pymunk.Vec2d(initialX, initialY)
 
         # Create a new rectangular bounding box with the given physics information and the given width and height
-        object_bounding_box = pymunk.Poly.create_box(physics_body, (width, height))
+        bounding_box = pymunk.Poly.create_box(physics_body, (width, height))
 
         # Set the collision type to the value of the enum given by the parameter
-        object_bounding_box.collision_type = collision_type.value
+        bounding_box.collision_type = collision_type.value
 
         # Add the physical object and its bounding box to the physics simulation space
-        self.physics_space.add(physics_body, object_bounding_box)
+        self.physic_environment.physics_space.add(physics_body, bounding_box)
 
         # Create the full object with physics and a sprite
-        completed_object = BoxSprite(object_bounding_box, sprite_path, width=width, height=height)
+        completed_object = BoxSprite(bounding_box=bounding_box,
+                                     filename=sprite_path,
+                                     width=width,
+                                     height=height)
 
         # Finally add the full object to the list of sprites in the scene
-        self.sprite_list.append(completed_object)
+        self.physic_environment.sprite_list.append(completed_object)
 
     def createPhysicalWindowBounds(self):
         """
@@ -111,32 +144,113 @@ class PhysicsEnvironment:
         """
 
         # Create the bottom bound
-        self.lines.createLine(body_type=pymunk.Body.STATIC,
-                              collision_type=CollisionType.STATIC_OBJECT,
-                              first_endpoint=(0, self.window_height),
-                              second_endpoint=(self.window_width,self.window_height),
-                              thickness=1)
+        self.physic_environment.lines.createLine(body_type=pymunk.Body.STATIC,
+                                                 collision_type=CollisionType.STATIC_OBJECT,
+                                                 first_endpoint=(0, self.physic_environment.window_height),
+                                                 second_endpoint=(self.physic_environment.window_width,
+                                                                  self.physic_environment.window_height),
+                                                 thickness=1)
 
         # Create the top bound
-        self.lines.createLine(body_type=pymunk.Body.STATIC,
-                              collision_type=CollisionType.STATIC_OBJECT,
-                              first_endpoint=(0, 0),
-                              second_endpoint=(self.window_width, 0),
-                              thickness=1)
+        self.physic_environment.lines.createLine(body_type=pymunk.Body.STATIC,
+                                                 collision_type=CollisionType.STATIC_OBJECT,
+                                                 first_endpoint=(0, 0),
+                                                 second_endpoint=(self.physic_environment.window_width, 0),
+                                                 thickness=1)
 
         # Create the right side bound
-        self.lines.createLine(body_type=pymunk.Body.STATIC,
-                              collision_type=CollisionType.STATIC_OBJECT,
-                              first_endpoint=(0, 0),
-                              second_endpoint=(0, self.window_height),
-                              thickness=1)
+        self.physic_environment.lines.createLine(body_type=pymunk.Body.STATIC,
+                                                 collision_type=CollisionType.STATIC_OBJECT,
+                                                 first_endpoint=(0, 0),
+                                                 second_endpoint=(0, self.physic_environment.window_height),
+                                                 thickness=1)
 
         # Create the left side bound
-        self.lines.createLine(body_type=pymunk.Body.STATIC,
-                              collision_type=CollisionType.STATIC_OBJECT,
-                              first_endpoint=(self.window_width, 0),
-                              second_endpoint=(self.window_width, self.window_height),
-                              thickness=1)
+        self.physic_environment.lines.createLine(body_type=pymunk.Body.STATIC,
+                                                 collision_type=CollisionType.STATIC_OBJECT,
+                                                 first_endpoint=(self.physic_environment.window_width, 0),
+                                                 second_endpoint=(self.physic_environment.window_width,
+                                                                  self.physic_environment.window_height),
+                                                 thickness=1)
 
 
+class DynamicPhysics:
 
+    def __init__(self, physics_environment: PhysicsEnvironment):
+        self.physics_environment = physics_environment
+
+    def createDynamicRectangularObject(self, width, height, mass, friction, dampening, initialX, initialY,
+                                       collision_type: CollisionType, sprite_path):
+        """
+        Create a new dynamic physics object
+
+        :param width: Width of object (PX)
+        :param height: Height of object (PX)
+        :param mass: The mass of the object
+        :param friction: How much friction the object should create
+        :param dampening: How quickly the velocity should fall off when it is no longer being applied
+        :param initialX: Object's initial X coordinate
+        :param initialY: Object's initial Y coordinate
+        :param collision_type: What kind of collision the object has
+        :param sprite_path: Path to the objects sprite
+
+        :return: The created object
+        """
+
+        # How likely the object is to rotate around its center
+        object_moment = pymunk.moment_for_box(mass=mass,
+                                              size=(width, height))
+
+        # Create an actual physics body with mass
+        physics_body = pymunk.Body(mass=mass,
+                                   moment=object_moment)
+
+        # Set the starting location of the object
+        physics_body.position = pymunk.Vec2d(initialX, initialY)
+
+        # Complete physics object (without a sprite)
+        bounding_box = pymunk.Poly.create_box(body=physics_body,
+                                              size=(width, height))
+
+        # The amount of friction the object should generate when it moves
+        bounding_box.friction = friction
+
+        # The type of collision this object has
+        bounding_box.collision_type = collision_type.value
+
+        # Add the object to the physics space
+        self.physics_environment.physics_space.add(physics_body, bounding_box)
+
+        # Finally create the entire object with a sprite
+        completed_object = DynamicObject(bounding_box=bounding_box,
+                                         sprite_path=sprite_path,
+                                         width=width,
+                                         height=height,
+                                         dampening=dampening)
+        return completed_object
+
+class DynamicObject(BoxSprite):
+    """Inherits from the Box Sprite to simply add more functionality to that object"""
+
+    def __init__(self, bounding_box, sprite_path, width, height, dampening):
+        """
+        Create a new object
+
+        :param bounding_box: The bounding box of the physics object
+        :param sprite_path: Path to the sprite being used
+        :param width: Width of the object
+        :param height: Height of the object
+        :param dampening: The amount of dampening used to slow down and object
+        """
+
+        super().__init__(bounding_box=bounding_box, filename=sprite_path, width=width, height=height)
+        self.dampening = dampening
+
+    def get_dampening(self):
+        """
+        Get the amount of dampening to be applied to the object
+
+        :return: Dampening amount
+        """
+
+        return self.dampening
